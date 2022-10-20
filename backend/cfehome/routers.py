e5a7d869a285655a65941a606b4c9139ff65790e
mumar <mumar@cybershellsol.com>
2022-10-20 19:53:00 +0500

    second

--- backend/cfehome/routers.py
+++ backend/cfehome/routers.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from products.viewsets import ProductViewSet
+from products.viewsets import ProductViewSet, ProductGenericViewSet
 
 router = DefaultRouter()
-router.register('products-abc', ProductViewSet)
+router.register('products-abc', ProductGenericViewSet)
 urlpatterns = router.urls
+print(urlpatterns)
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,19 +1,74 @@
 from dataclasses import field
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
+from api.serializers import UserPublicSerializer
 from .models import Product
+from .  import validators
 
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source='user', read_only=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name="product-detail",
+        lookup_field="pk"
+    )
+    title = serializers.CharField(validators=[
+        validators.validate_title_no_hello, 
+        validators.unique_product_title
+    ])
+
+    # name = serializers.CharField(source='title', read_only=True)
+    email = serializers.EmailField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
-            'title', 
+            'owner', 
+            'url',
+            'edit_url',
+            'email',
+            'pk',
+            'title',
+            # 'name',
             'content', 
             'price', 
             'sale_price', 
-            'discount'
+            'discount',
+            'my_user_data'
         ]
+
+    def get_my_user_data(self, obj):
+        return {
+            'username':obj.user.username
+        }
+
+    # def validate_title(self, value):
+    #     qs = Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
+
+    # def create(self, validated_data):
+    #     # return Product.objects.create(**validated_data)
+    #     # email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     # print(email, ";;;;", obj)
+    #     return obj
+
+    # def update(self, instance, validated_data):
+    #     email = validated_data.pop('email')
+
+    #     # instance.title = validated_data.get('title')
+    #     return super().update(instance, validated_data)
+    
+    def get_edit_url(self, obj):
+        # return f"/api/products/{obj.pk}/"
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={'pk':obj.pk}, request=request)
     
     def get_discount(self, obj):
         if not hasattr(obj, 'id'):
